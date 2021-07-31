740b61063346842e887f4538eff87a45005aa614
scarymen1325 <kyslenko3@outlook.com>
2021-07-31 17:03:12 +0300

    sm6250-common: add lineage overlay
    * Without that, notification LED not work
    * rename "overlay-arrow" to "overlay-system"

--- common.mk
+++ common.mk
@@ -285,7 +285,8 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-system \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
